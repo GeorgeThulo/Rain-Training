04fc5c85af44ef6d0d657396c593bf45fea86b61
GeorgeThulo <Ofentsethulo@hotmail.com>
2024-06-21 12:30:37 +0200

    Dump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Select, Store } from '@ngxs/store';
+import { RandomJokesState } from './store/states/random-jokes.state';
+import { Observable } from 'rxjs';
+import { RandomJokesResponse } from './store/types/random-jokes-response.interface';
+import { CategoryJokesAction, RandomJokesActions } from './store/actions'; 
+import { CategoryJokeState } from './store/states/category-jokes.state';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,31 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Implement-Ngxs';
+
+  @Select(RandomJokesState.getData) data$!: Observable<RandomJokesResponse> // change name
+  @Select(RandomJokesState.isLoading) isLoading$!: Observable<boolean>
+  @Select(CategoryJokeState.getData) category$!: Observable<string[]>
+
+  //title = 'WEIRDO';
+  //first_name = ''; 
+  //last_name = '';
+  //joke ='';
+
+  constructor(
+    private store: Store,
+    private http: HttpClient,
+  ) {}
+
+  onJokeCategory(){
+    this.store.dispatch(new CategoryJokesAction.Fetch())
+  }
+
+  onRandomJoke(){
+    this.store.dispatch(new RandomJokesActions.Fetch())
+  }
+  
+  onClear(){
+    this.store.dispatch(new RandomJokesActions.Clear())    
+  }   
+     
 }
